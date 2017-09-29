5378bed23788c2a4cac874888edd8b954a52e3fc
Michael Romero <romerom@gmail.com>
2017-09-29 02:51:30 -0700

    carousel working correctly

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,11 +1,12 @@
-
 $(document).ready(function() {
   console.log('document loaded');
   $('.carousel_class').slick({
     dots: true,
-    variableWidth: true
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    adaptiveHeight: true
   });
-
 });
 
 $(window).on("load", function() {
