8e9e6bb3922b03b09803959ce99b895a4f25ac9d
Michael Romero <romerom@gmail.com>
2017-09-30 16:39:19 -0700

    for the cloud

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ console.log("Server running at http://localhost:%d", port);
 // Deployd mounts its server on process.server. This means you can only run one Deployd server in a process.
 // Deployd loads resources from the process.cwd. Add this to ensure you are in the right directory: process.chdir(__dirname).
 // In order to access the /dashboard without a key you must run Deployd with the env option set to development.
-var deployd = require('deployd')
-  , options = {port: 3000};
-var dpd = deployd(options);
-dpd.listen();
+//var deployd = require('deployd')
+  //, options = {port: 3000};
+//var dpd = deployd(options);
+//dpd.listen();
