e79ef22084f7bc3224af92d28ded8c0c56a705f4
Michael Romero <romerom@gmail.com>
2017-09-29 21:04:47 -0700

    derp

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,11 @@
 $(document).ready(function() {
   console.log('document loaded');
   $('.carousel_class').slick({
-    dots: true,
+    dots: false,
     infinite: true,
     speed: 300,
     slidesToShow: 1,
     variableWidth: true,
     centerMode: true
-  });
-
-  var calendar = $('#calendar').calendar({
-    tmpl_path: '/tmpls/',
-    modal: '#events-modal',
-    events_source: '/testOutput.json'
   });
 });
