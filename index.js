10cca97bd2cdc48e58068928ed24257672ca2057
Michael Romero <romerom@gmail.com>
2017-09-30 19:49:40 -0700

    one more time for the cloud

--- index.js
+++ index.js
@@ -3,17 +3,5 @@ var deployd = require('deployd')
   , options = {"port": port};
 
 var dpd = deployd(options);
-
 dpd.listen();
-
-
-
-
-//var http = require('http');
-//var server = http.createServer(function(request, response) {
-    //response.writeHead(200, {"Content-Type": "text/plain"});
-    //response.end("Hello World!");
-//});
-//var port = process.env.PORT || 1337;
-//server.listen(port);
 console.log("Server running at http://localhost:%d", port);
