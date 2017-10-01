a9f154a41f6881580cdb8973b61d96f5ff31fd86
Michael Romero <romerom@gmail.com>
2017-09-30 19:43:03 -0700

    daldfjasdf

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-var http = require('http');
-var server = http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
-});
 var port = process.env.PORT || 1337;
-server.listen(port);
+var deployd = require('deployd')
+  , options = {"port": port};
+
+var dpd = deployd(options);
+
+dpd.listen();
+
+
+
+
+//var http = require('http');
+//var server = http.createServer(function(request, response) {
+    //response.writeHead(200, {"Content-Type": "text/plain"});
+    //response.end("Hello World!");
+//});
+//var port = process.env.PORT || 1337;
+//server.listen(port);
 console.log("Server running at http://localhost:%d", port);
