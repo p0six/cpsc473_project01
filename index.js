57b464d61d1d00a0f5244cb168e6ebb29f652920
Michael Romero <romerom@gmail.com>
2017-10-01 23:00:05 -0700

    mongo for the cloud

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 var port = process.env.PORT || 1337;
-var MONGODB_URI = process.env.MONGODB_URI;
+//var MONGODB_URI = process.env.MONGODB_URI;
 var deployd;
-if (MONGODB_URI === undefined) {
+//if (MONGODB_URI === undefined) {
   deployd = require('deployd')
     , options = {
     "port": port,
     "env": "development"
   };
-  console.log("MONGODB_URI is undefined");
-} else {
-  deployd = require('deployd')
-    , options = {
-    "port": port,
-    "db.connectionString": MONGODB_URI,
-    "env": "development"
-  };
-  console.log("MONGODB_URI is defined: " + MONGODB_URI);
-}
+  //console.log("MONGODB_URI is undefined");
+//} else {
+  //deployd = require('deployd')
+    //, options = {
+    //"port": port,
+    //"db.connectionString": MONGODB_URI,
+    //"env": "development"
+  //};
+  //console.log("MONGODB_URI is defined: " + MONGODB_URI);
+//}
 
 var dpd = deployd(options);
 dpd.listen();
