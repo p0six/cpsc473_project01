5470b674ea37d453a47e3d767d153ef0495f5547
Danial Mohammad <dmohammad@ieeci.com>
2017-10-15 21:03:30 -0700

    Going backward a bit before pulling to merge. Attendees and attending list should be working.

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,5 @@
-/* global $ */
-var dpd = window.dpd;
-
 $(document).ready(function() {
   console.log('document loaded');
-
   // Login Buttons
   dpd.users.me(function(user) {
     if (user) {
@@ -13,24 +9,20 @@ $(document).ready(function() {
       $('#signInButton').show();
       $('#logOutButton').hide();
     }
-
-    function initCarousel() {
-      $('.carousel_class').slick({
-        dots: false,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        variableWidth: true
-        //variableWidth: true,
-        /*swipe: true,
-        swipeToSlide: true,
-        touchMove: true,*/
-        //centerMode: true
-      });
-    }
-    window.initCarousel = initCarousel;
-    initCarousel();
-
   });
-  // Carousel initialization moved into 'app.js' in Calendar's onAfterEventsLoad function..
+
+  $('#attending').click(function() {
+    id = $(this).attr('data-eventid');
+    alert(id);
+  });
+
+  // Carousel
+  $('.carousel_class').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    variableWidth: true,
+    centerMode: true
+  });
 });
