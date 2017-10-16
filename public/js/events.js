07c47b560bb43b64d9c1ee4605e077d92b0cef16
Michael Romero <romerom@gmail.com>
2017-10-16 03:48:05 -0700

    image upload

--- public/js/events.js
+++ public/js/events.js
@@ -7,11 +7,12 @@ $('.form_datetime').datetimepicker({
 });
 
 $('#showEvent').click(function() {
-	$('#attending-modal').modal('hide');
-	$('#eventModal').modal('show');
+  $('#attending-modal').modal('hide');
+  $('#eventModal').modal('show');
 });
 
 var rg_owner;
+var rg_img_url = '/upload/default-no-image.png';
 
 $('#submitEvent').click(function() {
   var startDate = new Date($('#event_start').val());
@@ -28,7 +29,7 @@ $('#submitEvent').click(function() {
 
   dpd.users.me(function(result, error) {
     if (error || !result) {
-			swal({
+      swal({
         title: 'Error Posting Event!',
         text: JSON.stringify(error),
         type: 'error',
@@ -46,10 +47,11 @@ $('#submitEvent').click(function() {
         streetAddress: rg_address,
         city: rg_city,
         statePrefix: rg_state,
-        zip: rg_zip
+        zip: rg_zip,
+        eventImage: rg_img_url
       }, function(result, error) {
         if (error)
-				  swal({
+          swal({
             title: 'Error Posting Event!',
             text: JSON.stringify(error),
             type: 'error',
@@ -61,7 +63,7 @@ $('#submitEvent').click(function() {
             userId: rg_owner
           });
         $('#event-modal').modal('hide');
-				swal({
+        swal({
           title: 'Event Successfully Added!',
           text: 'Now go invite some friends!',
           type: 'success',
@@ -72,3 +74,64 @@ $('#submitEvent').click(function() {
   });
   console.log('return');
 });
+
+
+var files = [];
+var uploadFiles = function() {
+  var fd = new FormData()
+  for (var i in files) {
+    fd.append('uploadedFile', files[i])
+  }
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', '/event-images?subdir=eventImages&uniqueFilename=true');
+  xhr.onload = function() {
+    var response = JSON.parse(this.responseText);
+    console.log(response);
+  };
+  xhr.send(fd);
+
+  //alert('Image Uploaded Successfully');
+  swal({
+    title: 'Image Uploaded Successfully!',
+    text: 'Horray!',
+    type: 'success',
+    confirmButtonText: 'Cool'
+  })
+  calendar.view();
+
+  //get image url
+  var req = new XMLHttpRequest();
+  req.onreadystatechange = function() {
+    if (req.readyState === 4) {
+      if (req.status === 200) {
+        document.body.classname = 'ok';
+        var res = JSON.parse(req.responseText);
+        var target = res[res.length - 1].filename;
+
+        rg_img_url = '/upload/eventImages/' + target;
+
+      } else {
+        document.body.classname = 'error';
+      }
+    }
+  };
+  req.open('get', '/event-images', true);
+  req.send(null);
+};
+
+var setFiles = function(element) {
+  console.log('files:', element.files);
+  // Turn the FileList object into an Array
+  files = [];
+  for (var i = 0; i < element.files.length; i++) {
+    files.push(element.files[i]);
+  }
+};
+
+var deleteFile = function(element, id) {
+  $(element).closest('tr').remove();
+  dpd.upload.del(id, function(data, status) {
+    $('.alert-success').show();
+    $('.alert-success').append('File removed!');
+  });
+};
